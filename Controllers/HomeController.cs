457f639419429310d8705c461794edec62745859
MeeyoMy <tiklovci@gmail.com>
2023-06-11 20:05:29 +0200

    Added automatic buffer extension

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,15 +6,15 @@ using System.Text.Json;
 
 namespace CC_ASP.Controllers
 {
-	public class HomeController : Controller
+    public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         public static List<StorageViewModel> storages = new List<StorageViewModel>();
         public static List<ItemCountClass> itemCounts = new List<ItemCountClass>();
 
         public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
+        {
+            _logger = logger;
         }
 
         public IActionResult Index()
@@ -29,146 +29,142 @@ namespace CC_ASP.Controllers
                 return NotFound();
             }
 
-            var itemCountClass = itemCounts.Find(x => x.ComputerName == ComputerName);
+            List<ItemCountClass> ic = new List<ItemCountClass>();
+            ic = itemCounts.FindAll(x => x.ComputerName == ComputerName);
 
-            if (itemCountClass == null)
+            if (ic == null)
             {
                 return NotFound();
             }
 
-            return View(itemCountClass);
+            return View(ic);
         }
 
         [Route("/ws")]
-		public async Task Get()
-		{
-			if (HttpContext.WebSockets.IsWebSocketRequest)
-			{
-				using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-				_logger.Log(LogLevel.Information, "WebSocket connection established");
-				await Echo(webSocket);
-			}
-			else
-			{
-				HttpContext.Response.StatusCode = 400;
-			}
-		}
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                _logger.Log(LogLevel.Information, "WebSocket connection established");
+                await Echo(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
 
-		public async Task Echo(WebSocket webSocket)
-		{
-			var buffer = new byte[1024 * 4];
-			var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-			_logger.Log(LogLevel.Information, "Message received from Client");
+        public async Task Echo(WebSocket webSocket)
+        {
+            int bufferSize = 1024*4;
+            var buffer = new byte[bufferSize];
+            var offset = 0;
+            var free = buffer.Length;
+            WebSocketReceiveResult result;
 
-			try
-			{
-				string s = UTF8Encoding.UTF8.GetString(buffer, 0, result.Count);
-				
-				if (s != null)
+            while (true)
+            {
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer, offset, free), CancellationToken.None);
+                _logger.Log(LogLevel.Information, "Message received from Client");
+                offset += result.Count;
+                free -= result.Count;
+                if (result.EndOfMessage) break;
+                if (free == 0)
                 {
-                    string PCname = s.Substring(0, s.IndexOf(','));
-                    string jsonString = s.Substring(s.IndexOf(',') + 1);
+                    var newSize = buffer.Length + bufferSize;
+                    var newBuffer = new byte[newSize];
+                    Array.Copy(buffer, 0, newBuffer, 0, offset);
+                    buffer = newBuffer;
+                    free = buffer.Length - offset;
+                }
+            }
 
-                    List<ItemCountClass> list = JsonSerializer.Deserialize<List<ItemCountClass>>(jsonString)!;
-					List<string> items = new List<string>();
+            try
+            {
+                ConvertWebSocketData(buffer, result);
 
-                    for (int i = 0; i < storages.Count; i++)
+                await webSocket.CloseOutputAsync(WebSocketCloseStatus.Empty, result.CloseStatusDescription, CancellationToken.None);
+                _logger.Log(LogLevel.Information, "WebSocket connection closed");
+            }
+            catch
+            {
+                ConvertWebSocketData(buffer, result);
+            }
+
+            storages.Sort(delegate (StorageViewModel s1, StorageViewModel s2) { return s1.count.CompareTo(s2.count) * -1; });
+        }
+
+        private void ConvertWebSocketData(byte[] buffer, WebSocketReceiveResult result)
+        {
+            string s = UTF8Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+            if (s != null)
+            {
+                string PCname = s.Substring(0, s.IndexOf(','));
+                string jsonString = s.Substring(s.IndexOf(',') + 1);
+
+                List<ItemCountClass> list = JsonSerializer.Deserialize<List<ItemCountClass>>(jsonString)!;
+                List<string> items = new List<string>();
+
+                for (int i = 0; i < storages.Count; i++)
+                {
+                    if (storages[i].name == PCname)
+                        storages.Remove(storages[i]);
+                }
+
+                int total = 0;
+                foreach (ItemCountClass svm in list)
+                {
+                    total += svm.count;
+                }
+
+                storages.Add(new StorageViewModel() { name = PCname, count = total });
+
+
+
+                foreach (ItemCountClass i in list)
+                {
+                    bool isInList = false;
+                    foreach (string str in items)
                     {
-                        if (storages[i].name == PCname)
-                            storages.Remove(storages[i]);
+                        if (i.name == str)
+                        {
+                            isInList = true;
+                        }
                     }
-
-					int total = 0;
-					foreach (ItemCountClass svm in list)
-					{
-						total += svm.count;
-					}
-
-					storages.Add(new StorageViewModel() { name = PCname, count = total });
-
-
-
-                    foreach (ItemCountClass i in list)
+                    if (!isInList)
                     {
-                        bool isInList = false;
-                        foreach (string str in items)
-                        {
-                            if (i.name == str)
-                            {
-                                isInList = true;
-                            }
-                        }
-                        if (!isInList)
-                        {
-                            items.Add(i.name);
-                        }
+                        items.Add(i.name);
                     }
+                }
 
-                    for (int i = 0; i < itemCounts.Count; i++)
+                for (int i = 0; i < itemCounts.Count; i++)
+                {
+                    foreach (string str in items)
                     {
-                        foreach (string str in items)
-                        {
-                            if (str == itemCounts[i].name) itemCounts.Remove(itemCounts[i]);
-                        }
+                        if (str == itemCounts[i].name) itemCounts.Remove(itemCounts[i]);
                     }
+                }
 
-                    int[] totalItems = new int[items.Count];
+                int[] totalItems = new int[items.Count];
 
-                    for (int i = 0; i < items.Count; i++)
+                for (int i = 0; i < items.Count; i++)
+                {
+                    foreach (ItemCountClass icc in list)
                     {
-                        foreach (ItemCountClass icc in list)
+                        if (icc.name == items[i])
                         {
-                            if (icc.name == items[i])
-                            {
-                                totalItems[i] += icc.count;
-                            }
+                            totalItems[i] += icc.count;
                         }
                     }
+                }
 
-					for(int i = 0; i < items.Count; i++)
-					{
-						itemCounts.Add(new ItemCountClass() { ComputerName = PCname, name = items[i], count = totalItems[i] });
-					}
-
-                    await webSocket.CloseOutputAsync(WebSocketCloseStatus.Empty, result.CloseStatusDescription, CancellationToken.None);
-					_logger.Log(LogLevel.Information, "WebSocket connection closed");
-				}
-			}
-			catch
-			{
-				string s = UTF8Encoding.UTF8.GetString(buffer, 0, result.Count);
-
-				if (s != null)
-				{
-					string PCname = s.Substring(0, s.IndexOf(','));
-					_logger.LogInformation(s.Substring(s.IndexOf(',') + 1));
-					List<StorageViewModel> list = JsonSerializer.Deserialize<List<StorageViewModel>>(s.Substring(s.IndexOf(',') + 1))!;
-
-					for (int i = 0; i < storages.Count; i++)
-					{
-						if (storages[i].name == PCname)
-							storages.Remove(storages[i]);
-					}
-
-					int total = 0;
-					foreach (StorageViewModel svm in list)
-					{
-						total += svm.count;
-					}
-
-					storages.Add(new StorageViewModel() { name = PCname, count = total });
-				}
-			}
-
-			storages.Sort(delegate (StorageViewModel s1, StorageViewModel s2) { return s1.count.CompareTo(s2.count) * -1; });
-		}
-
-
-
-
-
-
-
-
-	}
+                for (int i = 0; i < items.Count; i++)
+                {
+                    itemCounts.Add(new ItemCountClass() { ComputerName = PCname, name = items[i], count = totalItems[i] });
+                }
+            }
+        }
+    }
 }
